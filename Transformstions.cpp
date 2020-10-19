519b53264a1a4f0435260215385890a15141fa87
Jahanvi Sharma <56484763+jahanvisharma-dotcom@users.noreply.github.com>
2020-10-19 23:52:24 +0530

    Update Transformstions.cpp

--- Transformstions.cpp
+++ Transformstions.cpp
@@ -1,1 +1,90 @@
+2D Translation:
 
+#include<graphics.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int graphdriver=DETECT,graphmode,errorcode;
+int i;
+int x2,y2,x1,y1,x,y;
+printf("Enter the 2 line end points:");
+printf("x1,y1,x2,y2");
+scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+initgraph(&graphdriver,&graphmode,"c:\\tc\\bgi");
+line(x1,y1,x2,y2);
+printf("Enter translation co-ordinates ");
+printf("x,y");
+scanf("%d%d",&x,&y);
+x1=x1+x;
+y1=y1+y;
+x2=x2+x;
+y2=y2+y;
+printf("Line after translation");
+line(x1,y1,x2,y2);
+getch();
+closegraph();
+}
+
+2D Rotation:
+
+#include<graphics.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int graphdriver=DETECT,graphmode,errorcode;
+int i;
+int x2,y2,x1,y1,x,y,xn,yn;
+double r11,r12,r21,r22,th;
+clrscr();
+printf("Enter the 2 line end points:");
+printf("x1,y1,x2,y2");
+scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+initgraph(&graphdriver,&graphmode,"c:\\tc\\bgi");
+line(x1,y1,x2,y2);
+printf("\n\n\n[ Enter the angle");
+scanf("%lf",&th);
+r11=cos((th*3.1428)/180);
+r12=sin((th*3.1428)/180);
+r21=(-sin((th*3.1428)/180));
+r22=cos((th*3.1428)/180);
+//printf("%lf  %lf  %lf  %lf",r11,r12,r21,r22);
+xn=((x2*r11)-(y2*r12));
+yn=((x2*r12)+(y2*r11));
+line(x1,y1,xn,yn);
+getch();
+closegraph();
+}
+
+
+2D Scaling:
+
+#include<graphics.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int graphdriver=DETECT,graphmode,errorcode;
+int i;
+int x2,y2,x1,y1,x,y;
+printf("Enter the 2 line end points:");
+printf("x1,y1,x2,y2");
+scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+initgraph(&graphdriver,&graphmode,"c:\\tc\\bgi");
+line(x1,y1,x2,y2);
+printf("Enter scaling co-ordinates ");
+printf("x,y");
+scanf("%d%d",&x,&y);
+x1=(x1*x);
+y1=(y1*y);
+x2=(x2*x);
+y2=(y2*y);
+printf("Line after scaling");
+line(x1,y1,x2,y2);
+getch();
+closegraph();
+}
